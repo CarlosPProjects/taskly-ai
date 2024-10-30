b889f844e4f77f3601f2cf82113ddb3328432b97
CarlosPProjects <cg9822@gmail.com>
2024-10-30 19:01:58 +0100

    "Removed empty lines from dashboard page and deleted chat-message component"

--- src/components/chatbot/chat-message.tsx
+++
@@ -1,9 +0,0 @@
-import ReactMarkdown from 'react-markdown';
-
-export default function ChatMessage({ content }) {
-  return (
-    <div className="chat-message">
-      <ReactMarkdown>{content}</ReactMarkdown>
-    </div>
-  );
-}
